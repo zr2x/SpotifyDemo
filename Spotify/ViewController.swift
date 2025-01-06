43e16bd550d39f4bd07fc3ada538757fdd1f0222
Iskander Sitdikov <master5212345@gmail.com>
2025-01-06 19:39:44 +0500

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Искандер Ситдиков on 06.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
